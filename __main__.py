c427b4acdf977a101bdca4a253fb0908d49e7432
come_ill_foo <rayveleaveme34@inbox.ru>
2023-06-14 01:13:38 +0100

    feat: basic commands

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import click, os, shutil, enum, errno, subprocess as sp
-
+from typing import Tuple
 
 class PatchResult(enum.Enum):
     OK = 0
@@ -29,7 +29,7 @@ def _validate_directory(ctx, param, value):
     return value
 
 
-def _patch(target: str, patch: str, extra_args: list[str]) -> int:
+def _patch(target: str, patch: str, extra_args: list[str]) -> PatchResult:
     p = sp.Popen(['patch', '-p1', '-F0', *extra_args, target],
                  stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
     stdout, stderr = '', ''
@@ -39,6 +39,7 @@ def _patch(target: str, patch: str, extra_args: list[str]) -> int:
     _echo(stderr.decode())
     if p.returncode == 0:
         return PatchResult.OK
+    # TODO: parse stdout for proper return codes
     return PatchResult.ERROR
 
 
@@ -58,7 +59,26 @@ def cli(verbose: bool):
 def deploy(directory: str, project_path: str):
     dira = os.path.join(directory, 'a')
     shutil.copytree(project_path, dira)
+    _echo(f'copied {project_path} to {dira}')
     shutil.copytree(dira, os.path.join(directory, 'b'))
+    _echo(f'replicated {dira} to b')
+    click.echo(f'{project_path} successfully deployed')
+
+
+def _redeploy(src: str, dst: str):
+    shutil.rmtree(dst)
+    shutil.copytree(src, dst)
+
+def _make_pathes(dir: str) -> Tuple[str, str]:
+    return (os.path.join(dir, 'a'), os.path.join(dir, 'b'))
+
+
+def _isdirs_or_die(*directories):
+    for directory in directories:
+        if not os.path.isdir(directory):
+            _echo(f'{directory} not found')
+            exit(errno.ENOENT)
+        _echo(f'{directory} exists')
 
 
 @cli.command()
@@ -68,13 +88,10 @@ def deploy(directory: str, project_path: str):
               help='Path where compared directories (a, b)')
 @click.argument('patches', nargs=-1,
                 type=click.Path(exists=True))
-def apply(directory: str, patches) -> int:
+def apply(directory: str, patches):
     fails = 0
-    dira = os.path.join(directory, 'a')
-    dirb = os.path.join(directory, 'b')
-    if not os.path.isdir(dira) or not os.path.isdir(dirb):
-        _echo(f'no a and b found in {directory}, run deploy firstly')
-        exit(errno.ENOENT)
+    dira, dirb = _make_pathes(directory)
+    _isdirs_or_die(dira, dirb)
 
     for patch in patches:
         _echo(f'applying {patch}...')
@@ -82,11 +99,61 @@ def apply(directory: str, patches) -> int:
         if rc != PatchResult.OK:
             click.echo(f'{NOTOK} {patch}')
             fails += 1
-            shutil.rmtree(dirb)
-            shutil.copytree(dira, dirb)
+            _redeploy(dira, dirb)
         else:
             click.echo(f'{OK} {patch}')
     exit(fails)
+
+
+def _revert(target: str, patch: str, extra_args: list[str]) -> PatchResult:
+    return _patch(target, patch, ['-R', *extra_args])
+
+
+@cli.command()
+@click.option('-C', '--directory', default=os.getcwd(),
+              type=click.Path(exists=True),
+              callback=_validate_directory,
+              help='Patched directory')
+@click.argument('patches', nargs=-1,
+                type=click.Path(exists=True))
+def revert(directory: str, patches):
+    for patch in reversed(patches):
+        _echo(f'reverting {patch}...')
+        rc = _revert(directory, patch, ['-d'])
+        if rc != PatchResult.OK:
+            click.echo(f'{NOTOK} {patch}')
+            exit(1)
+        else:
+            click.echo(f'{OK} {patch}')
+
+
+@cli.command()
+@click.option('-C', '--directory', default=os.getcwd(),
+              type=click.Path(exists=True),
+              callback=_validate_directory,
+              help='Path where compared directories (a, b)')
+@click.argument('patches', nargs=-1,
+                type=click.Path(exists=True))
+def dehunk(directory: str, patches):
+    fails = 0
+    dira, dirb = _make_pathes(directory)
+    _isdirs_or_die(dira, dirb)
+    for patch in patches:
+        _echo(f'applying {patch}...')
+        rc = _patch(dirb, patch, ['-d'])
+        if rc == PatchResult.HUNK_SUCCEED:
+            # with hunks to .old
+            shutil.copyfile(patch, patch + '.old')
+            # TODO: generate patch from diff
+            click.echo(f'{OK} {patch}')
+        elif rc == PatchResult.OK:
+            _echo(f'no hunks {patch} SKIP')
+        else:
+            fails += 1
+            _redeploy(dira, dirb)
+            click.echo(f'{NOTOK} {patch}')
+    exit(fails)
+
 
 
 if __name__ == '__main__':
